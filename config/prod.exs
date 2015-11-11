d4cf91eb7891b4440decbcf99b1851a80c04b1f1
tubedude <rtrevisan@gmail.com>
2015-11-11 00:06:11 -0200

    Fixing typo: missing comma

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :hello_phoenix, HelloPhoenix.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "calm-gorge-6173.herokuapp.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
